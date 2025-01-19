5e30928fe3e5bd55f22a3ede2cc67060cd3a4254
Rijan Shiwakoti <rijankrishna14@gmail.com>
2025-01-19 15:27:43 +0545

    Bugs Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
 import { useState } from "react";
 import { useEffect } from "react";
-import JObs from "./JObs";
+import Lists from "./Lists";
 
-const url = 'https://www.course-api.com/react-tabs-project';
+const url = "https://www.course-api.com/react-tabs-project";
 
 const App = () => {
-  const [isLoading,setIsLoading] = useState(true);
-  const [companyJobs,setCompanyJobs] = useState(null)
-  const fetchData = async()=>{
+  const [isLoading, setIsLoading] = useState(true);
+  const [companyJobs, setCompanyJobs] = useState(null);
+  const fetchData = async () => {
     try {
       const data = await fetch(url);
       const response = await data.json();
-      if(!data.ok)
-        {throw new error("Url does not exists");
-        }
+      if (!data.ok) {
+        throw new error("Url does not exists");
+      }
       setCompanyJobs(response);
-      setIsLoading(false)
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
     }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  if (isLoading) {
+    return <div className="loading"></div>;
   }
-  useEffect(()=>{
-
-    fetchData()
-  }
-  ,[])
-  if(isLoading){
-    return <div className="loading"></div>
-  }
-  return <div>
-    <JObs companyJobs={companyJobs}/>
-  </div>;
+  return (
+    <div>
+      <Lists companyJobs={companyJobs} />
+    </div>
+  );
 };
 export default App;
